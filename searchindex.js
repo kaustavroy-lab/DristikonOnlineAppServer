a2d8904f793a00d2d480ad914ea94f30ca5f941b
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2020-07-04 22:36:42 +0530

    new commit

--- searchindex.js
+++ searchindex.js
@@ -13,7 +13,7 @@ function SearchPage(url, title, keywords, description)
 function SearchDatabase()
 {
    database_length = 0;
-   this[database_length++] = new SearchPage("index.html", "Untitled Page", "Dance  Popular Bengali Songs  Bengali Poems  Hindi Songs  English Songs  In Your Language  Interviews  Short films  Popular Videos  Audio-Drama/ Story Plays  Dristikon Online App 2.0 © Copyright 2020 Dristikon, Tarakeswar  Designed & Developed by Kaustav Roy   ", "");
+   this[database_length++] = new SearchPage("index.html", "Untitled Page", "Drawing  Cooking  Dance  Popular Bengali Songs  Bengali Poems  Hindi Songs  English Songs  In Your Language  Interviews  Short films  Popular Videos  Audio-Drama/ Story Plays  Dristikon Online App 2.0 © Copyright 2020 Dristikon, Tarakeswar  Designed & Developed by Kaustav Roy   ", "");
    this[database_length++] = new SearchPage("page1.html", "Untitled Page", " ", "");
    this[database_length++] = new SearchPage("page2.html", "Untitled Page", " ", "");
    return this;
