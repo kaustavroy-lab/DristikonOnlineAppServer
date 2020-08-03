ae720869e815743578bf3646fce84d2096b12da6
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2020-08-03 09:03:39 +0530

    new commit

--- searchindex.js
+++ searchindex.js
@@ -13,6 +13,6 @@ function SearchPage(url, title, keywords, description)
 function SearchDatabase()
 {
    database_length = 0;
-   this[database_length++] = new SearchPage("index.html", "Dristikon Online App 2.3", "Drawing⮚  Cooking⮚  Dance⮚  Popular Bengali Songs⮚  Bengali Poems⮚  Hindi Songs⮚  English Songs⮚  In Your Language⮚  Interviews⮚  Short films⮚  Popular Videos⮚  Audio-Drama/ Story Plays⮚  Dristikon Online App 2.3 © Copyright 2020 Dristikon, Tarakeswar  Designed & Developed by Kaustav Roy  1  Val Lage Na   Episode-2  Awkward Conversations with Partners   Dristikon Online  CONTENT DETAILS  Val Lage Na Episode 2  Awkward Conversations with Partners  Casts  Jagnaseni & Soudip Script- Gourav  Direction- Soumyadyuti VFX and Editing- Kaustav Roy   ", "");
+   this[database_length++] = new SearchPage("index.html", "Dristikon Online App 2.3", "Cooking  Drawing  Dance  English Songs  Hindi Songs  Bengali Poems  Popular Bengali Songs  Interviews  Audio-Drama/ Story Plays  Short films  In Your Language  Popular Videos   1  Val Lage Na   Episode-2  Awkward Conversations with Partners   Dristikon Online  CONTENT DETAILS  Val Lage Na Episode 2  Awkward Conversations with Partners  Casts  Jagnaseni & Soudip Script- Gourav  Direction- Soumyadyuti VFX and Editing- Kaustav Roy  Dristikon Online App 2.3 © Copyright 2020 Dristikon, Tarakeswar  Designed & Developed by Kaustav Roy   ", "");
    return this;
 }
